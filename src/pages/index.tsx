fa23710b5a539b36b57bf8d55421b2dfb0f580a4
Araz Ashkan Abedi <arazabedi@hotmail.com>
2023-04-16 21:36:23 +0100

    navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
-import { SignIn, SignedIn, SignedOut, SignInButton, UserButton, SignOutButton } from "@clerk/nextjs";
+import {
+  SignIn,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignOutButton,
+} from "@clerk/nextjs";
 import Link from "next/link";
+import Navbar from "../components/Navbar";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export default function Home() {
-
-	const SignInPage = () => (
-		<SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-		);
+  const SignInPage = () => (
+    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+  );
 
   return (
     <main>
@@ -15,19 +25,21 @@ export default function Home() {
       </SignedIn>
       <SignedOut>
         {/* Signed out users get sign in button */}
-				<p>Holiday App Signed Out Page</p>
-				<button><Link href="/sign-in">sign-in</Link></button>
+        <p>Holiday App Signed Out Page</p>
+        <button>
+          <Link href="/sign-in">sign-in</Link>
+        </button>
       </SignedOut>
     </main>
   );
 }
 
-function Navbar() {
+function Something() {
   return (
     <main>
       <h1>Holiday App</h1>
       <UserButton />
-			<SignOutButton />
+      <SignOutButton />
     </main>
   );
 }
