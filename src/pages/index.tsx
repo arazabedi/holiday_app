3ba7ce098134119b643ce06cdba25a5a9123c9d9
Araz Ashkan Abedi <arazabedi@hotmail.com>
2023-04-16 21:03:48 +0100

    Auth fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,33 @@
+import { SignIn, SignedIn, SignedOut, SignInButton, UserButton, SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 export default function Home() {
+
+	const SignInPage = () => (
+		<SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+		);
+
   return (
     <main>
+      <SignedIn>
+        {/* Mount the UserButton component */}
+        <Navbar />
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+				<p>Holiday App Signed Out Page</p>
+				<button><Link href="/sign-in">sign-in</Link></button>
+      </SignedOut>
     </main>
-  )
+  );
+}
+
+function Navbar() {
+  return (
+    <main>
+      <h1>Holiday App</h1>
+      <UserButton />
+			<SignOutButton />
+    </main>
+  );
 }
